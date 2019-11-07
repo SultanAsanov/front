3e3d0997e14fd4ac72fdbdc5afbc0aa14daa5ea7
SultanAsanov <55678319+SultanAsanov@users.noreply.github.com>
2019-11-07 08:31:18 +0600

    Create Lesson2.js

---
+++ Lesson2.js
@@ -0,0 +1,25 @@
+let images=document.getElementById("images");
+let imageButton=document.getElementById("image_button");
+let imageText=document.getElementById("image_text");
+imageButton.addEventListener("click", () =>{
+  let iframe=document.createElement("img");
+  iframe.setAttribute("src", imageText.value);
+  images.appendChild(iframe);
+});
+let links=document.getElementById("links");
+let linkButton=document.getElementById("link_button");
+let linkText=document.getElementById("link_text");
+linkButton.addEventListener("click", () =>{
+  let iframe=document.createElement("A");
+  iframe.innerHTML = "Your link";
+  iframe.setAttribute("href", linkText.value);
+  links.appendChild(iframe);
+});
+let texts=document.getElementById("texts");
+let textButton=document.getElementById("text_button");
+let textText=document.getElementById("text_text");
+textButton.addEventListener("click", () =>{
+  let iframe=document.createElement("P");
+  iframe.innerHTML = textText.value;
+  texts.appendChild(iframe);
+});
